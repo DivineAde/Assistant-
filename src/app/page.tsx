e689b413cb1142d1a489ed1dc9ea63ac29bf0f1e
Divine Adeyeye <87317463+DivineAde@users.noreply.github.com>
2023-10-02 16:10:25 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Country from "./components/Country";
+import Oasis from "./components/Country";
 import { Shiba } from "./components/Shiba";
 
 export default function Home() {
   return (
     <main className="">
       <Shiba />
-      <Country />
+      <Oasis />
     </main>
   );
 }
